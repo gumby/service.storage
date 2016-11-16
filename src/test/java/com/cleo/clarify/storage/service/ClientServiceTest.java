178d9ba7f0e716e7d726733ff728dbc255f2052e
pgombola <pgombola@pgombola-mbp.extol.com>
2016-11-16 16:08:34 -0500

    Client code cleanup.

--- src/test/java/com/cleo/clarify/storage/service/ClientServiceTest.java
+++ src/test/java/com/cleo/clarify/storage/service/ClientServiceTest.java
@@ -18,53 +18,59 @@ import rx.Observable;
 
 public class ClientServiceTest {  
 
-  private static final int numChunks = 100_900;
+  private static final int numChunks = 105_000;
+  private static final int minBytes = 16_384;
   private static final int maxBytes = 65_572;
-  
+
   public static void main(String[] args) throws InterruptedException {
     ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 9090).usePlaintext(true).build();
     AtomicBoolean shutdown = new AtomicBoolean();
-    StreamObserver<ObjectData> data = StorageServiceGrpc.newStub(channel).store(new StreamObserver<ObjectInfo>() {
+    StreamObserver<ObjectData> data = StorageServiceGrpc.newStub(channel).store(new ObjectInfoResponseObserver(shutdown));
 
-      @Override
-      public void onCompleted() {
-        System.out.println("Response completed.");
-        shutdown.set(true);
-      }
-
-      @Override
-      public void onError(Throwable t) {
-        t.printStackTrace();
-      }
-
-      @Override
-      public void onNext(ObjectInfo info) {
-        System.out.println(info);
-        System.out.println("To gb: " + FileUtils.byteCountToDisplaySize(info.getObjectSize()));
-      }
-    });
-    
-    IntStream stream = new Random().ints(numChunks, 16384, maxBytes);
-    Observable.from(stream::iterator)
+    IntStream chunkSizes = new Random().ints(numChunks, minBytes, maxBytes);
+    Observable.from(chunkSizes::iterator)
     .forEach(
-        size -> {
-          data.onNext(ObjectData.newBuilder().setData(ByteString.copyFrom(getRandBytes(size))).build());
-        },
+        size -> data.onNext(ObjectData.newBuilder().setData(ByteString.copyFrom(randomBytes(size))).build()),
         error -> { 
           data.onError(error);
           error.printStackTrace();
         },
-        () -> {
-          data.onCompleted();
-        });
+        () -> data.onCompleted());
+
     while (!shutdown.get());
   }
-  
-  private static byte[] getRandBytes(int size) {
+
+  private static byte[] randomBytes(int size) {
     Random rand = new Random();
     byte[] bytes = new byte[size];
     rand.nextBytes(bytes);
     return bytes;
   }
-  
+
+  private static final class ObjectInfoResponseObserver implements StreamObserver<ObjectInfo> {
+
+    private AtomicBoolean shutdown;
+    
+    public ObjectInfoResponseObserver(AtomicBoolean shutdown) {
+      this.shutdown = shutdown;
+    }
+
+    @Override
+    public void onCompleted() {
+      System.out.println("Response completed.");
+      shutdown.set(true);
+    }
+
+    @Override
+    public void onError(Throwable t) {
+      t.printStackTrace();
+    }
+
+    @Override
+    public void onNext(ObjectInfo info) {
+      System.out.println(info);
+      System.out.println("Size: " + FileUtils.byteCountToDisplaySize(info.getObjectSize()));
+    }
+  }
+
 }
