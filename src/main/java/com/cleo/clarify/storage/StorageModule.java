df2eb828c6c2b2a7d069241441f8a17dc1ecb2c4
pgombola <pgombola@pgombola-mbp.extol.com>
2016-11-17 16:46:05 -0500

    Building actual session connection instead of mock

--- src/main/java/com/cleo/clarify/storage/StorageModule.java
+++ src/main/java/com/cleo/clarify/storage/StorageModule.java
@@ -1,14 +1,21 @@
 package com.cleo.clarify.storage;
 
-import org.mockito.Mockito;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import com.cleo.clarify.storage.repository.CassandraStorageRepository;
 import com.cleo.clarify.storage.repository.StorageRepository;
+import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.Session;
+import com.google.common.net.HostAndPort;
 import com.google.inject.AbstractModule;
+import com.google.inject.Inject;
 import com.google.inject.Provides;
 import com.google.inject.Scopes;
 import com.google.inject.Singleton;
+import com.typesafe.config.Config;
 
 public class StorageModule extends AbstractModule {
 
@@ -19,7 +26,14 @@ public class StorageModule extends AbstractModule {
 
   @Provides
   @Singleton
-  public Session sessionProvider() {
-    return Mockito.mock(Session.class);
+  @Inject
+  public Session sessionProvider(Config config) {
+    List<InetAddress> contacts = config.getStringList("cassandra.contacts").stream()
+      .map(contact -> HostAndPort.fromString(contact))
+      .map(hostAndPort -> new InetSocketAddress(hostAndPort.getHostText(), hostAndPort.getPort()).getAddress())
+      .collect(Collectors.toList());
+    Cluster cluster = Cluster.builder().addContactPoints(contacts).build();
+    
+    return cluster.connect(config.getString("cassandra.keyspace"));
   }
 }
